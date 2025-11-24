8b105cd594b1bba492ab4af44c1aaf6158dcfeef
Soumya Kar <soumyakar.39@hotmail.com>
2025-11-24 23:12:39 +0530

    feat: add dynamic faker imports for random data generation in tests; update package.json and package-lock.json to include faker dependency; remove unused waitAndTimeout.spec.ts

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test } from "@playwright/test";
 import { PageManager } from "../page-objects/pageManager";
+// import {faker} from '@faker-js/faker' // Replaced with dynamic import below
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
@@ -16,10 +17,13 @@ test("navigate to form page", async ({ page }) => {
 
 test('parameterized method', async ({ page })=>{
     const pm = new PageManager(page);
+    const { faker } = await import('@faker-js/faker');
+    const randomFullName = faker.person.fullName();
+    const randonEmail = randomFullName.replace(/ /g, "") + faker.number.int(1000)+ '@test.com';
 
     await pm.navigateTo.formLayoutPage();
     await pm.onFormLayoutPage.submitUsingGridForm('test@test.com', 'welcome1', 'Option 1' );
-    await pm.onFormLayoutPage.submitInlineForm('Karan Arjun', 'kararn@test.com', true);
+    await pm.onFormLayoutPage.submitInlineForm(randomFullName, randonEmail, true);
     await pm.navigateTo.datePickerPage();
     await pm.onDatePickerPage.selectCommonDatePickerDate(14);
     await pm.onDatePickerPage.selectDatePickerWithRangeFromToday(10, 15);
